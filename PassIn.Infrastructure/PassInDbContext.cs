0a01ef1d95b1e5582f23ef6ae66798c89a886025
Carlos Eduardo <ducameneses16@gmail.com>
2024-04-22 01:11:23 -0300

    feat: Add a dbset for check_ins

--- PassIn.Infrastructure/PassInDbContext.cs
+++ PassIn.Infrastructure/PassInDbContext.cs
@@ -1,11 +1,13 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using PassIn.Infrastructure.Entities;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace PassIn.Infrastructure;
 public class PassInDbContext : DbContext
 {
     public DbSet<Event> Events { get; set; }
     public DbSet<Attendee> Attendees { get; set; }
+    public DbSet<CheckIn> Check_Ins { get; set; }
 
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
