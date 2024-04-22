abea0bd2bca05c4947e7c9b82978275214e9a699
Carlos Eduardo <ducameneses16@gmail.com>
2024-04-22 00:56:54 -0300

    feat: Add an entity to check in

---
+++ PassIn.Infrastructure/Entities/CheckIn.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PassIn.Infrastructure.Entities;
+public class CheckIn
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public DateTime Created_at { get; set; }
+    public Guid AttendeeId { get; set; }
+    public Attendee Attendee { get; set; } = default!;
+    
+}
